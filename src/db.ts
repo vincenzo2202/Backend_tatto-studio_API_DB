4a9668a6ea2bb409ed5b728eddb943b1573ba9e9
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2023-10-25 21:32:38 +0200

    feat: migration with roles table created

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { UsersTableMigration1698262021256 } from "./migration/1698262021256-users_table_migration"
+import { RolesTableMigration1698262215631 } from "./migration/1698262215631-roles_table_migration"
 
 export const AppDataSource = new DataSource({
  type: "mysql",
@@ -10,7 +11,7 @@ export const AppDataSource = new DataSource({
  password: "1234",
  database: "tattoo_studio_backend_db",
  entities: [],
- migrations:[UsersTableMigration1698262021256],
+ migrations:[UsersTableMigration1698262021256,RolesTableMigration1698262215631],
  synchronize: false,
  logging: false,
 })
---
+++ src/migration/1698262215631-roles_table_migration.ts
@@ -0,0 +1,49 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm"
+
+export class RolesTableMigration1698262215631 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "roles",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment",
+                    },
+                    {
+                        name: "role",
+                        type: "enum",
+                        enum: ["user", "admin", "super_admin"],
+                        default: `"user"`
+                    },
+                    {
+                        name: "privilege",
+                        type: "varchar",
+                        length: "255",
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "CURRENT_TIMESTAMP"
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "CURRENT_TIMESTAMP",
+                        onUpdate: "CURRENT_TIMESTAMP"
+                    },
+                ],
+            }),
+            true
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("roles")
+    }
+
+}
