695160addcfd7cb73354e3da5d44a8e93e3581b5
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2023-10-25 22:39:05 +0200

    feat: models Portfolio_worker created using typeorm library

---
+++ src/models/Portfolio_worker.ts
@@ -0,0 +1,20 @@
+import { Entity, BaseEntity, PrimaryGeneratedColumn, Column } from "typeorm"
+
+@Entity("portfolio")
+export class Portfolio extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id!: number
+  
+    @Column()
+    portfolio_id!: number
+
+    @Column()
+    worker_id!: number
+    
+    @Column()
+    created_at!: Date
+
+    @Column()
+    updated_at!: Date
+
+}
