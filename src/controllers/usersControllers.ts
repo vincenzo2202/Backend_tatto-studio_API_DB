1b420cf5289d4f697b7753f8b803af0f31e8d911
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2023-10-28 18:55:16 +0200

    refactor:remove unnecessary code for improved performance

--- src/controllers/usersControllers.ts
+++ src/controllers/usersControllers.ts
@@ -197,7 +197,6 @@ const updateUser = async (req: Request, res: Response) => {
         const bodyUser = req.body
         const id = req.token.id
 
-        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
         const passwordRegex = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{4,12}$/;
 
         if (typeof (bodyUser.full_name) !== "string") {
